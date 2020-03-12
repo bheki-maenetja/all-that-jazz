7c6641274c1bbf2bf42bc4a7f94f4964c0886d03
bheki-maenetja <bhekimaenetja@gmail.com>
2020-03-12 11:30:23 +0000

    modified serializers for playlists

--- backend/playlists/models.py
+++ backend/playlists/models.py
@@ -8,6 +8,7 @@ class Playlist(models.Model):
   name = models.CharField(max_length=50)
   description = models.CharField(max_length=300)
   owner = models.ForeignKey(User, related_name='playlists', null=False, on_delete=models.CASCADE)
+  songs = models.ManyToManyField('songs.Song', related_name='playlists', blank=True)
 
   def __str__(self):
     return f'{self.name} (Id: {self.id})'
--- backend/playlists/serializers.py
+++ backend/playlists/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import Playlist
 
+from songs.models import Song
+from songs.models import Artist
+
 User = get_user_model()
 
 # Native Serializers
@@ -18,6 +21,20 @@ class UserSerializer(serializers.ModelSerializer):
     model = User
     fields = ('id', 'username', 'email', 'first_name', 'last_name', 'profile_image')
 
+class ArtistSerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model = Artist
+    exclude = ('description', )
+
+class SongSerializer(serializers.ModelSerializer):
+
+  artist = ArtistSerializer()
+  class Meta:
+    model = Song
+    exclude = ('categories', 'audio_preview_url')
+
 # Populated Native Serializers
 class PopulatedPlaylistSerializer(PlayListSerializer):
   owner = UserSerializer()
+  songs = SongSerializer(many=True)
