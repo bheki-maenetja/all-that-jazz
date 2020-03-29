15b21b149c241141d7694d2a3677e1ffcaceb998
bheki-maenetja <bhekimaenetja@gmail.com>
2020-03-29 12:18:34 +0200

    ready for deployment

---
+++ frontend/build/precache-manifest.1435d52eb9f1becafef05725a251a255.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1b7c2ae9eb3f4c149fee7f11e2973f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8957f2b02bfbe296b829",
+    "url": "/static/css/2.d6d97064.chunk.css"
+  },
+  {
+    "revision": "f83faf87cbbeaba64fe9",
+    "url": "/static/css/main.0b7f054c.chunk.css"
+  },
+  {
+    "revision": "8957f2b02bfbe296b829",
+    "url": "/static/js/2.0ac6ffc8.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.0ac6ffc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83faf87cbbeaba64fe9",
+    "url": "/static/js/main.dc0444cf.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "08864aa1441f8c0e2bb5b193ca0e8fee",
+    "url": "/static/media/artistIndexBackground.08864aa1.jpg"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/static/media/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "19647afdfabdbebf65edb134c5af8e24",
+    "url": "/static/media/homePageBackground-1.19647afd.jpg"
+  },
+  {
+    "revision": "33d7706145a58269fd9fc89151a27b39",
+    "url": "/static/media/homePageBackground-2.33d77061.jpg"
+  },
+  {
+    "revision": "eef9e651d942f8ed8b9c3bf313f908f3",
+    "url": "/static/media/homePageBackground-3.eef9e651.jpg"
+  },
+  {
+    "revision": "ddad8f446a2e6dacaa16c3cb0b5b0238",
+    "url": "/static/media/homePageBackground-4.ddad8f44.jpg"
+  },
+  {
+    "revision": "7ac5ccb11884608d2820bd330d50587a",
+    "url": "/static/media/login-card-img.7ac5ccb1.png"
+  },
+  {
+    "revision": "030624dd310fabdf7fd459f8c6d12032",
+    "url": "/static/media/profilePageBackground.030624dd.jpg"
+  },
+  {
+    "revision": "aaa901a01555486c4a5eae823881c3d8",
+    "url": "/static/media/register-card-img.aaa901a0.jpg"
+  },
+  {
+    "revision": "6b65c48752c7d6e76eee5496c8bf56cf",
+    "url": "/static/media/songIndexBackground.6b65c487.jpg"
+  }
+]);
--- project/urls.py
+++ project/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('jwt_auth.urls')),
     path('api/playlists/', include('playlists.urls')),
     path('api/artists/', include('artists.urls')),
-    path('api/songs/', include('songs.urls'))
+    path('api/songs/', include('songs.urls')),
+    re_path(r'^.*$', index)
 ]
